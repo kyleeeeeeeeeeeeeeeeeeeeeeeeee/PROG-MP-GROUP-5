e2bd35894973f15b773079c176e5cf322abac7ee
kyleeeeeeeeeeeeeeeeeeeeeeeeee <161435410+kyleeeeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-06-27 21:59:46 +0800

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -1,3 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define MAX_CINEMAS 10
+#define MAX_SHOWINGS 6
+#define MAX_SEATS 50
+
+typedef struct {
+    char title[100];
+    char description[256];
+    int running_time; 
+    char show_times[MAX_SHOWINGS][10]; 
+    char seats[MAX_SHOWINGS][MAX_SEATS]; 
+} Movie;
+
+typedef Movie Cinema[MAX_CINEMAS];
+
+void loadSched(char *filename)
+{
+  
+}
+
+void saveExit(char *filename)
+{
+  
+}
+
+void viewSched()
+{
+  
+}
+
+void displaySeat(int cinemaNum, char *showTime)
+{
+  
+}
+
+void selectSeat(int cinemaNum, char *showTime)
+{
+  
+}
+
+void searchTitle(char *title)
+{
+  
+}
+
+void searchTime(char *time)
+{
+
+}
+
+void printTicket(int cinema_number, char *title, char *showTime, char *seatNum)
+{
+
+}
+
+int checkAvailability(int cinemaNum, char *showTime, char *seatNum)
+{
+
+}
+
+void markTaken(int cinemaNum, char *showTime, char *seatNum)
+{
+
+}
+
+void mainMenu()
+{
+
+}
+
 
