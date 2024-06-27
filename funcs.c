173ca90692600eb413b12e5602a89ea272145e92
kyleeeeeeeeeeeeeeeeeeeeeeeeee <161435410+kyleeeeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-06-27 22:40:30 +0800

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -80,7 +80,38 @@ void markTaken(int cinemaNum, char *showTime, char *seatNum)
 
 void mainMenu()
 {
+ int choice, cinemaNum;
+ char filename, showTime, title, time, seatNum;
+    do{
+        printf("1. Load Schedule\n");
+        printf("2. View Schedule\n");
+        printf("3. Select Seats\n");
+        printf("4. Search Movie\n");
+        printf("5. Print Ticket\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
+        switch (choice) {
+            case 1:
+                loadSched(&filename);
+                break;
+            case 2:
+                viewSched();
+                break;
+            case 3:
+                selectSeat(cinemaNum, &showTime);
+                break;
+            case 4:
+               searchTitle(&title);
+               searchTime(&time);
+               break;
+            case 5:
+                printTicket(cinemaNum, &title, &showTime, &seatNum);
+                break;
+            case 6:   
+                // Exit
+        } while(choice != 6);
 }
 
 
