d5ca0721fedb96fb42885cbb7353439b7c272eea
kyleeeeeeeeeeeeeeeeeeeeeeeeee <161435410+kyleeeeeeeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-06-28 19:07:03 +0800

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -6,6 +6,7 @@
 #define MAX_CINEMAS 10
 #define MAX_SHOWINGS 6
 #define MAX_SEATS 50
+
 typedef char longstr[101];
 typedef char str[31];
 
@@ -20,14 +21,68 @@ struct showTag{
     str showingTime;
     str seats[5][10];
 }
-struct ViewMovieTag{    //main tag: structure for showing movies (1 showing time = 1 cinema)
+struct ViewMovieTag{    // main tag: structure for showing movies (1 showing time = 1 cinema)
     struct MovieTag Movie;
     struct showTag show[15];
 };
 
+struct ViewMovieTag cinemas[MAX_CINEMAS]; // Array of cinema schedules
+
 void loadSched(char *filename)
 {
-  
+   int i, j, k, cinemaIndex = 0;
+   FILE *fp; 
+   fp = fopen(filename, "r");
+    if (fp == NULL) 
+    {
+        printf("Unable to open file: %s\n", filename);
+        return;
+    }
+    while (fscanf(fp, "%d\n", &cinemaIndex) != EOF) 
+    {
+        if (cinemaIndex >= MAX_CINEMAS) 
+        {
+            printf("Cinema index %d out of bounds\n", cinemaIndex);
+            fclose(fp);
+            return;
+        }
+
+    while (!feof(fp)) 
+    {                
+        if (fscanf(fp, "%d\n", &cinemaIndex) == 1) 
+        {
+            if (cinemaIndex < MAX_CINEMAS) 
+            {    
+                // Read movie title, description, and runtime from file
+                fscanf(fp, "%30[^\n]\n", cinemas[cinemaIndex].Movie.title);
+                fscanf(fp, "%100[^\n]\n", cinemas[cinemaIndex].Movie.description);
+                fscanf(fp, "%30[^\n]\n", cinemas[cinemaIndex].Movie.runTime);
+
+                // Read showing times and initialize seat availability
+                for (i = 0; i < MAX_SHOWINGS; i++) 
+                {
+                    if (fscanf(fp, "%30[^\n]\n", cinemas[cinemaIndex].show[i].showingTime) == 1) 
+                    {
+                        // Initialize seats to 'O' indicating available
+                        for (j = 0; j < 5; j++) 
+                        {
+                            for (k = 0; k < 10; k++) 
+                            {
+                                strcpy(cinemas[cinemaIndex].show[i].seats[j][k], "O");
+                            }
+                        }
+                    }
+                }
+            } 
+            else 
+            {
+                printf("Cinema index %d out of bounds\n", cinemaIndex);
+                fclose(fp);
+                return;
+            }
+        }
+    }
+   fclose(fp);
 }
 
 void saveExit(char *filename)
